8e034ff9f7578001225ad6ab89be715597cbef93
Jack Rivera <jackarivera@gmail.com>
2023-07-02 00:13:57 -0400

    Fix urdf not showing in simulation and rviz

--- roverrobotics_description/launch/display_miti_payload.launch.py
+++ roverrobotics_description/launch/display_miti_payload.launch.py
@@ -2,7 +2,7 @@ from ament_index_python.packages import get_package_share_path
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
